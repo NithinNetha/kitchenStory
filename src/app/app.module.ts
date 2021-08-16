5bafb69de26d01c64fff28d33e1043790e4deb9f
Nithin Palarapu <nithin6425@gmail.com>
2021-08-16 16:03:09 +0530

    Created Json and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
